1e9d0ff8f75a0ce9be1bef779ee89577ba117bd0
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-23 16:14:33 +0100

    aggiunta logica mail

--- Mail/script.js
+++ Mail/script.js
@@ -1,13 +1,19 @@
-var mail = ["angelo", "francesco@gmail.com", "giovanni@gmail.com", "marco@live.it", "alice@virgilio.it"];
+var mail = ["angelo@live.it", "francesco@gmail.com", "giovanni@gmail.com", "marco@live.it", "alice@virgilio.it"];
 
 console.log(mail);
 
 var utente = prompt('Inserire mail per accedere');
 
-if (mail == utente) {
-    alert("Utente accettato");
-} else {
-    alert("Accesso negato");
-}
-
 console.log(utente);
+
+for (var i=0; i < 5; i++) {
+    if (mail[i] == utente) {
+        alert("Utente accettato");
+        break;
+    } else {
+        alert("Accesso negato");
+        break;
+    }
+};
+
+// break interrompe il ciclo
